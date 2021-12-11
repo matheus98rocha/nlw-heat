6195851fc2e046bc63c7fd8b7921b4f14c6cf236
Matheus Gonçalves Rocha <matheus98rocha@gmail.com>
2021-12-11 15:55:36 -0300

    Backend

---
+++ backend/src/app.ts
@@ -0,0 +1,33 @@
+import "dotenv/config";
+import express, { application, response } from 'express';
+import http from 'http';
+import { Server } from 'socket.io';
+
+
+import { router } from "./routes";
+
+const app = express();
+
+const serverHttp = http.createServer(app);
+
+const io = new Server(serverHttp);
+
+app.use(express.json());
+
+app.use(router);
+
+app.get("/github", (request, response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENTE_ID}`);
+});
+
+app.get('/signin/callback', (request, response) => {
+    const { code } = request.query;
+
+    return response.json(code);
+
+
+});
+
+app.listen(4000, () => {
+    console.log(`🚀 Server is running on port 4000`);
+})
---
+++ backend/src/routes.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import { AuthenticateUserController } from './controllers/AuthenticateUserController';
+import { CreateMessageController } from './controllers/CreateMessageController';
+import { ensureAutheticated } from './middleware/ensureAuthenticated';
+
+const router = Router();
+
+router.post("/authenticate", new AuthenticateUserController().handle);
+
+router.post("/messages", ensureAutheticated, new CreateMessageController().handle);
+
+export { router }
